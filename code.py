8b1976d520f636774d4fbdc10c39185a70d508ff
Lakshay_Edicts <77452480+LakshayEdicts@users.noreply.github.com>
2021-03-30 07:30:03 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+stock = 600
+jeans_sold = 500
+target = 500
+
+target_hit = jeans_
+sold == target
+print("Hit jeans sale target")
+print(target_hit)
+
+current_stock = stock - jeans_sold
+in_stock = current_stock !=0
+print("Jeans In Stock")
+print(in_stock)
